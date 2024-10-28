779099277cfb15b20ba6e40df57cfa1e0c6a29bf
StephaneApuhin <146180697+StephaneApuhin@users.noreply.github.com>
2024-10-27 23:16:27 -0700

    rose.php

---
+++ rose.php
@@ -0,0 +1,10 @@
+<?php
+// file1.php - Rose
+$flowerName = "Rose";
+$flowerColor = "Red";
+$flowerMeaning = "Love and Romance";
+
+echo "Flower: " . $flowerName . "<br>";
+echo "Color: " . $flowerColor . "<br>";
+echo "Meaning: " . $flowerMeaning . "<br>";
+?>
