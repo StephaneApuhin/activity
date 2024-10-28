7faf04c43bb2641b17a7002c9f93d0e72021e4a1
StephaneApuhin <146180697+StephaneApuhin@users.noreply.github.com>
2024-10-27 23:17:00 -0700

    tulip.php

---
+++ tulip.php
@@ -0,0 +1,10 @@
+<?php
+// file2.php - Tulip
+$flowerName = "Tulip";
+$flowerColor = "Yellow";
+$flowerMeaning = "Happiness and Cheerfulness";
+
+echo "Flower: " . $flowerName . "<br>";
+echo "Color: " . $flowerColor . "<br>";
+echo "Meaning: " . $flowerMeaning . "<br>";
+?>
