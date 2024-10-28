796ad5706098d0f2074607164ca88df6d0419798
StephaneApuhin <146180697+StephaneApuhin@users.noreply.github.com>
2024-10-27 23:17:31 -0700

    lily.php

---
+++ lily.php
@@ -0,0 +1,10 @@
+<?php
+// file3.php - Lily
+$flowerName = "Lily";
+$flowerColor = "White";
+$flowerMeaning = "Purity and Refinement";
+
+echo "Flower: " . $flowerName . "<br>";
+echo "Color: " . $flowerColor . "<br>";
+echo "Meaning: " . $flowerMeaning . "<br>";
+?>
