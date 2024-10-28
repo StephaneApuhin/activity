5716a5289d4f1d6279d5e2ed236d23c3e5fad39a
StephaneApuhin <146180697+StephaneApuhin@users.noreply.github.com>
2024-10-27 23:17:58 -0700

    orchid.php

---
+++ orchid.php
@@ -0,0 +1,10 @@
+<?php
+// file4.php - Orchid
+$flowerName = "Orchid";
+$flowerColor = "Purple";
+$flowerMeaning = "Beauty and Strength";
+
+echo "Flower: " . $flowerName . "<br>";
+echo "Color: " . $flowerColor . "<br>";
+echo "Meaning: " . $flowerMeaning . "<br>";
+?>
